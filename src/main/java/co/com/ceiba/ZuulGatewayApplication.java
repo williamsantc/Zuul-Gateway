b17c0fcf4a5d2f946c50ecc90ceccd44dc000d47
william.santos <william.santos@ceiba.com.co>
2019-06-21 20:52:21 -0500

    inicio del proyecto

---
+++ src/main/java/co/com/ceiba/ZuulGatewayApplication.java
@@ -0,0 +1,17 @@
+package co.com.ceiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+}
